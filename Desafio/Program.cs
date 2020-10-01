411b830b4f37fe25b454a518a4c9e1ac7a816837
edijunior88 <er.moura88@yahoo.com.br>
2020-09-30 22:11:54 -0300

    Adicionar arquivos de projeto.

--- Desafio/Program.cs
+++ Desafio/Program.cs
@@ -6,6 +6,21 @@ namespace Desafio
     {
         static void Main(string[] args)
         {
+
+            /*
+             Use as duas técnicas diferentes para imprimir as duas linhas da saída. 
+            Em outras palavras, você aprendeu a exibir uma mensagem em apenas uma 
+            linha de código e exibir uma mensagem usando várias linhas de código. 
+            Use as duas técnicas para esse desafio. Não importa qual técnica você 
+            aplicará a qual linha e não importa por quantas maneiras você dividirá 
+            uma das mensagens em várias linhas de código. Isso fica a seu critério.
+            
+            Não importa como você fará isso; o código deverá produzir a saída a seguir.
+            
+            This is the first line.
+            This is the second line.
+             */
+
             Console.WriteLine("Essa é a primeira linha.");
             Console.WriteLine("Essa é a segunda linha.");
         }
