b86b8af84acd19553c9bb1cc9d3afe8d6c32c8cb
edijunior88 <er.moura88@yahoo.com.br>
2020-10-01 21:39:01 -0300

    Adicionar arquivos de projeto.

---
+++ variavel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Para criar uma nova variável, primeiro você deve declarar 
+            o tipo de dados da variável e, em seguida, dar um nome a ela.
+             */
+
+            string primeiroNome;
+
+            /*
+             Aqui estão alguns exemplos de declarações de variáveis usando 
+            os tipos de dados sobre os quais aprendemos anteriormente.
+             */
+
+            char userOption;
+            int gameScore;
+            decimal particulesPerMillion;
+            bool processesCustomer;
+        }
+    }
+}
