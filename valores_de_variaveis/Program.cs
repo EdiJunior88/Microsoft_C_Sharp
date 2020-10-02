aa4f96591c5eae88774aa19651437120f0eaacc0
edijunior88 <er.moura88@yahoo.com.br>
2020-10-01 21:54:19 -0300

    Exercício - Configurando e obtendo valores de variáveis

--- valores_de_variaveis/Program.cs
+++ valores_de_variaveis/Program.cs
@@ -10,6 +10,24 @@ namespace valores_de_variaveis
     {
         static void Main(string[] args)
         {
+
+            string primeiroNome;
+            primeiroNome = "Bob";
+            Console.WriteLine(primeiroNome);
+
+            primeiroNome = "Beth";
+            Console.WriteLine(primeiroNome);
+
+            primeiroNome = "Conrad";
+            Console.WriteLine(primeiroNome);
+
+            primeiroNome = "Grant";
+            Console.WriteLine(primeiroNome);
+
+            string value = "Bob";
+            Console.WriteLine(value);
+
+            Console.ReadKey();
         }
     }
 }
