f71437060afd9ba52ec111173982b740eb6fd714
edijunior88 <er.moura88@yahoo.com.br>
2020-10-15 16:18:16 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosFixacao/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            1 - Um programa de uma determinada empresa tem o objetivo de salvar o ​nome completo ​de
+            todos os funcionários que estiveram presentes no trabalho em todos os dias do mês, 
+            para que sejam entregues as devidas bonificações. Segundo o enunciado acima, assinale 
+            o tipo de dado que corresponda ao armazenamento dessa variável:
+
+            R: String
+         
+            2 - Um operador de caixa em um mercado utiliza um programa que o informa todos os ​preços
+            dos produtos disponíveis para a venda, com base nisso, informe quais seriam os tipos de
+            dados que essas variáveis armazenam:
+
+            R: Float, Double
+      
+            3 - Um bibliotecário utiliza um programa que organiza em ordem alfabética os livros por 
+            sua letra inicial, que tipo de variável esse programa utiliza?
+
+            R: Char
+          
+            4- Em um software de detecção de mentiras, qual tipo de variável pode ser utilizado para 
+            armazenar os dados de afirmações verdadeiras e falsas?
+
+            R: Bool
+         
+             5- Pedro está desenvolvendo um aplicativo que gerencia um grupo de membros nas redes 
+            sociais, porém ele não sabe quais tipos de variáveis ele deve usar para armazenar cada 
+            dado da aba de cadastro ao qual o app solicita para a entrada de novos membros. Conforme 
+            o enunciado acima e os conhecimentos adquiridos na aula, Assinale a alternativa correta
+            referente aos tipos de dados em cada opção:
+            
+            ● Insira seu​ ​Nome​.
+            ● Informe sua ​Idade.
+            ● Informe o ​número total de visualizações ​no app.
+            ● Primeira ​letra ​do seu nome.
+            ● Está de acordo com todas as políticas do grupo?​ ​SIM​ ​OU ​NÃO
+
+            R: I - string, II - int, III - float, IV - char, V - bool
+             */
+
+        }
+    }
+}
